fac40449169f65d7723b95d3750c4972a12c94d7
Ankush <ankush4031@gmail.com>
2024-02-09 20:35:32 +0530

    giving style to calculator

---
+++ calculator/src/App.jsx
@@ -0,0 +1,37 @@
+import styles from "./App.module.css";
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+import { useState } from "react";
+
+function App() {
+
+  const [calVal , setCalVal] = useState("");
+
+  const onButtonClick = (buttonText) => {
+    console.log(buttonText);
+    if(buttonText === 'C'){
+      setCalVal("");
+    }
+    else if(buttonText === '='){
+      const result = eval(calVal);
+      setCalVal(result);
+    }
+    else{
+      const newDisplayVal = calVal + buttonText;
+      setCalVal(newDisplayVal);
+    }
+
+  }
+
+
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.calculator}>
+        <Display displayVal={calVal} />
+        <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+      </div>
+    </div>
+  );
+}
+
+export default App
---
+++ calculator/src/components/ButtonsContainer.jsx
@@ -0,0 +1,33 @@
+import styles from "./ButtonsContainer.module.css";
+
+const ButtonsContainer = ({ onButtonClick }) => {
+  const buttonNames = [
+    "C",
+    "B",
+    "%",
+    "/",
+    "7",
+    "8",
+    "9",
+    "*",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    ".",
+    "0",
+    "()",
+    "=",
+  ];
+
+  return (
+    <div className={styles.buttonContainer}>
+      {buttonNames.map( buttonName => <button className={styles.button} key={buttonName} onClick={() => onButtonClick(buttonName)}>{buttonName}</button>)}
+    </div>
+  );
+};
+export default ButtonsContainer;
